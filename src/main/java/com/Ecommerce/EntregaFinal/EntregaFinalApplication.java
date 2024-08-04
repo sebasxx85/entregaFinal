f0050b360a73be244fac0070b2391c6e0bd465d0
sebaxx85 <isurus.1313@gmail.com>
2024-08-04 00:33:46 -0400

    primer commit

---
+++ src/main/java/com/Ecommerce/EntregaFinal/EntregaFinalApplication.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.EntregaFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregaFinalApplication.class, args);
+	}
+
+}
